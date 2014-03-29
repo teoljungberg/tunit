aca546bafe4efcd300bd8421525fb34ff6f32e75
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-03-29 16:31:37 +0100

    CompoundReporter gathers all other reporters
    Since they all quack the same

---
+++ lib/rtest/compound_reporter.rb
@@ -0,0 +1,33 @@
+require 'rtest/reporter'
+
+module Rtest
+  class CompoundReporter < Reporter
+    def initialize *reporters
+      super()
+      self.reporters = reporters
+    end
+    attr_accessor :reporters
+
+    def << reporter
+      self.reporters << reporter
+    end
+
+    def passed?
+      self.reporters.all?(&:passed?)
+    end
+
+    def start
+      self.reporters.each(&:start)
+    end
+
+    def record result
+      self.reporters.each do |reporter|
+        reporter.record result
+      end
+    end
+
+    def report
+      self.reporters.each(&:report)
+    end
+  end
+end
