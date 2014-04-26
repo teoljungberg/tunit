fb2b753e3a923a726d8273c6d12362dd27742dfc
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-04-26 06:30:17 +0200

    Move Tunit::Spec::DSL to a file of it's own

--- lib/tunit/spec.rb
+++ lib/tunit/spec.rb
@@ -1,4 +1,4 @@
-require 'tunit/test'
+require 'tunit/spec/dsl'
 
 module Kernel
   # Override describe to avoid warnings and collisions with minitest/spec
@@ -20,65 +20,6 @@ end
 
 module Tunit
   class Spec < Test
-    module DSL
-      attr_reader :name
-
-      def before &block
-        define_method :setup do
-          self.instance_eval(&block)
-        end
-      end
-
-      def after &block
-        define_method :teardown do
-          self.instance_eval(&block)
-        end
-      end
-
-      def it desc, &block
-        block ||= -> { skip "(no tests defined)" }
-
-        @specs ||= 0
-        @specs += 1
-
-        test_name = desc.gsub " ", "_"
-        name      = "test_%04d_%s" % [ @specs, test_name ]
-
-        define_method name, &block
-
-        name
-      end
-
-      def let name, &block
-        define_method(name) {
-          @lazy ||= {}
-          @lazy.fetch(name) {|method| @lazy[method] = instance_eval(&block) }
-        }
-      end
-
-      def children
-        @children ||= []
-      end
-
-      def create name
-        klass = Class.new(self) {
-          @name = name
-
-          remove_test_methods!
-        }
-
-        children << klass
-
-        klass
-      end
-
-      def remove_test_methods!
-        self.runnable_methods.map { |test|
-          self.send :undef_method, test
-        }
-      end
-    end
-
     extend DSL
   end
 end
