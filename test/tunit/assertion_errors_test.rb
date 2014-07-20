028b86a2a909782122fd5089b4e4e3616d9a9845
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-07-20 19:42:09 +0200

    Rename `assertion` to `assertion_error`

--- test/tunit/assertion_errors_test.rb
+++ test/tunit/assertion_errors_test.rb
@@ -5,83 +5,83 @@ require 'tunit/test'
 module Tunit
   class AssertionErrorTest < TestCase
     def setup
-      self.assertion = Assertion.new
+      self.assertion_error = Assertion.new
     end
-    attr_accessor :assertion
+    attr_accessor :assertion_error
 
     def test_error
-      assert_instance_of Assertion, assertion.error
+      assert_instance_of Assertion, assertion_error.error
     end
 
     def test_location
-      result       = FailingTest.new.run
-      assertion    = result.failure
-      exp_location = %r(test/tunit/test_case.rb:\d{1,})
+      result          = FailingTest.new.run
+      assertion_error = result.failure
+      exp_location    = %r(test/tunit/test_case.rb:\d{1,})
 
-      assert_match exp_location, assertion.location
+      assert_match exp_location, assertion_error.location
     end
 
     def test_result_code
-      assert_equal "F", assertion.result_code
+      assert_equal "F", assertion_error.result_code
     end
 
     def test_result_label
-      assert_equal "Failure", assertion.result_label
+      assert_equal "Failure", assertion_error.result_label
     end
   end
 
   class SkipErrorTest < TestCase
     def setup
-      self.assertion = Skip.new
+      self.assertion_error = Skip.new
     end
-    attr_accessor :assertion
+    attr_accessor :assertion_error
 
     def test_error
-      assert_instance_of Skip, assertion.error
+      assert_instance_of Skip, assertion_error.error
     end
 
     def test_location
-      result       = SkippedTest.new.run
-      assertion    = result.failure
-      exp_location = %r(test/tunit/test_case.rb:\d{1,})
+      result          = SkippedTest.new.run
+      assertion_error = result.failure
+      exp_location    = %r(test/tunit/test_case.rb:\d{1,})
 
-      assert_match exp_location, assertion.location
+      assert_match exp_location, assertion_error.location
     end
 
     def test_result_code
-      assert_equal "S", assertion.result_code
+      assert_equal "S", assertion_error.result_code
     end
 
     def test_result_label
-      assert_equal "Skipped", assertion.result_label
+      assert_equal "Skipped", assertion_error.result_label
     end
   end
 
   class ErrorErrorTest < TestCase
     def setup
-      self.assertion = Error.new
+      self.assertion_error = Error.new
     end
-    attr_accessor :assertion
+    attr_accessor :assertion_error
 
     def test_error
-      assert_instance_of Error, assertion.error
+      assert_instance_of Error, assertion_error.error
     end
 
     def test_location
-      result       = ErrorTest.new.run
-      assertion    = result.failure
-      exp_location = %r(test/tunit/test_case.rb:\d{1,})
+      result          = ErrorTest.new.run
+      assertion_error = result.failure
+      exp_location    = %r(test/tunit/test_case.rb:\d{1,})
 
       assert_instance_of NotAnAssertion, result.failure
-      assert_match exp_location, assertion.location
+      assert_match exp_location, assertion_error.location
     end
 
     def test_result_code
-      assert_equal "E", assertion.result_code
+      assert_equal "E", assertion_error.result_code
     end
 
     def test_result_label
-      assert_equal "Error", assertion.result_label
+      assert_equal "Error", assertion_error.result_label
     end
   end
 end
