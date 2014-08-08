cbd0140173b151998ef45e8f4359b2d1cf603fa9
Teo Ljungberg <teo.ljungberg@gmail.com>
2014-08-08 14:38:19 +0200

    Move `Kernel#describe` to Spec::DSL

--- lib/tunit/spec.rb
+++ lib/tunit/spec.rb
@@ -1,25 +1,6 @@
 require 'tunit/spec/dsl'
 require 'tunit/spec/expect'
 
-module Kernel
-  # Override describe to avoid warnings and collisions with minitest/spec
-  alias_method :_old_describe, :describe if defined? Minitest
-
-  def describe desc, &block
-    _old_describe desc, &block if defined? Minitest
-
-    super_klass = if Class === self && is_a?(Tunit::Spec::DSL)
-                    self
-                  else
-                    Tunit::Spec
-                  end
-
-    klass = super_klass.create_sub_klass desc
-    klass.class_eval(&block)
-    klass
-  end
-end
-
 module Tunit
   class Spec < Test
     extend DSL
