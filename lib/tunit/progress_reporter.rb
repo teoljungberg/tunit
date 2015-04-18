65b67112e26cd0986dcd02bc6f81718c9d2e51fb
Teo Ljungberg <teo@teoljungberg.com>
2015-04-18 15:55:11 +0200

    Extract verbose printing to private method

--- lib/tunit/progress_reporter.rb
+++ lib/tunit/progress_reporter.rb
@@ -4,8 +4,15 @@ module Tunit
   class ProgressReporter < Reporter
     def record result
       io.print result.code
-      io.print " = %s#%s (%.2f s)" % [result.class, result.name, result.time] if options[:verbose]
-      io.puts if options[:verbose]
+      print_progress result if options[:verbose]
+    end
+
+    private
+
+    def print_progress result
+      io.print " = %s#%s (%.2f s)" %
+        [result.class, result.name, result.time]
+      io.puts
     end
   end
 end
